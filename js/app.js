f2c7d96d29636c0131137420e22630189f9221a6
Jason Ziegler <jason@insightfulautomation.com>
2021-05-01 17:18:27 -0500

    declared generateHTML function and called in fetch

--- js/app.js
+++ js/app.js
@@ -7,20 +7,23 @@ const userAPI = 'https://randomuser.me/api/?results=12'
 // code from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 fetch(userAPI)
   .then(response => response.json())
-  .then(data => console.log(data.results));
+  .then(generateHTML);
 
-// display the results to the screen in their cards
-
-/*
-<div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+// HELPER FUNCTIONS
+function generateHTML(data) {
+    console.log(data.results);
+    // display the results to the screen in their cards
+    /*
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">first last</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+        </div>
     </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-</div>
-*/
+    */
+}
 
